20d8a351e36e1e4ac1aed1393d065c4c81c52f39
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-21 07:47:03 +1000

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,29 +1,43 @@
 import openai
 import streamlit as st
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# Retrieve the OpenAI API key from Streamlit's secrets
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
+# Set the title and caption for the Streamlit app
 st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI LLM")
+
+# Initialize session state variable to store the conversation history
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-for msg in st.session_state.messages:
+# Display the conversation history
+for msg in st.session_state["messages"]:
     st.chat_message(msg["role"]).write(msg["content"])
 
+# Get user input and proceed if the input is not None or empty
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Set the OpenAI API key
+    openai.api_key = openai_api_key  
+    
+    # Add user message to session state and display it in the chat
+    st.session_state["messages"].append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    
+    # Show a loading indicator while the bot is fetching a response
+    with st.spinner("Assistant is thinking..."):
+        try:
+            # Call OpenAI API to get a response based on the conversation history
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state["messages"][-20:])
+            msg = response.choices[0].message
+            
+            # Add assistant's response to session state and display it in the chat
+            st.session_state["messages"].append(msg)
+            st.chat_message("assistant").write(msg.content)
+        except openai.error.OpenAIError as e:
+            # Handle API errors gracefully
+            st.error(f"API error: {e}")
+        except Exception as e:
+            # Handle other potential errors gracefully
+            st.error(f"An unexpected error occurred: {e}")
